a191c352f7d4efc953189342002e08123eaaa274
SHRISTI <singh.shristi0411@gmail.com>
2026-07-20 16:31:56 +0530

    Refactor comments and clean up code in test_data.py

--- test_data.py
+++ test_data.py
@@ -1,19 +1,14 @@
 import pandas as pd
-
-# Load the files
+#load the files
 matches = pd.read_csv('matches.csv')
 deliveries = pd.read_csv('deliveries.csv')
 players= pd.read_csv('players.csv')
-
 print("MATCHES.CSV COLUMNS")
 print(matches.columns.tolist())
-
 print("\nDELIVERIES.CSV COLUMNS")
 print(deliveries.columns.tolist())
-
 print("\nPLAYERS.CSV COLUMNS")
 print(players.columns.tolist())
-
 print("\nDATA PREVIEW")
 print("Matches Head:")
 print(matches.head(1)) 
@@ -22,10 +17,8 @@ h2h = deliveries.groupby(['batter_name', 'bowler_name']).agg({
     'batsman_run': 'sum',
     'id': 'count' )
 }).reset_index()
-
 h2h.columns = ['batter_name', 'bowler_name', 'h2h_runs', 'h2h_balls']
-
-#Calculate H2H Strike Rate
+#h2h sr
 h2h['h2h_strike_rate'] = (h2h['h2h_runs'] / h2h['h2h_balls']) * 100
 
 # 5. Save this to use in your App
