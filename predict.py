97ae69dd844476d151af9d2936b03b1457a9ad3e
SHRISTI <singh.shristi0411@gmail.com>
2026-07-20 16:30:04 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
+#to check the prediction by providing all data to the model
 import pickle
 import pandas as pd
-
 pipe = pickle.load(open('pipe.pkl', 'rb'))
 scenario = pd.DataFrame({
     'batting_team': ['Royal Challengers Bengaluru'],
@@ -13,20 +13,15 @@ scenario = pd.DataFrame({
     'striker_rating': [25],
     'bowler_rating': [35]
 })
-# index [0][1] is the probability for the Batting Team (RCB)
-# index [0][0] is the probability for the Bowling Team (MI)
 probabilities = pipe.predict_proba(scenario)
 rcb_win = probabilities[0][1] * 100
 mi_win = probabilities[0][0] * 100
-
-#output
 print("LIVE MATCH PREDICTION")
 print(f"Target: 167 | Need: 9 runs from 3 balls | Wickets Left: 2")
 print("-" * 30)
 print(f"RCB Win Probability: {rcb_win:.2f}%")
 print(f"MI Win Probability: {mi_win:.2f}%")
 print("-" * 30)
-
 if rcb_win > mi_win:
     print("Prediction: RCB is likely to pull off the win!")
 else:
